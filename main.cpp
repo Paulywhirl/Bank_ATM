c751f22747c68181d3b0b4f1dea3e083c1e5427d
Paul Henderson <phender9@uwo.ca>
2016-10-14 14:39:01 -0400

    Added a small foundation. Most notably User class and main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <string.h>
+#include <fstream>
+#include "User.h"
 using namespace std;
 
 int main() {
 
+
+    bool user = false, user_type = false;
+    int type;
+    User new_user;
+
     cout << "Welcome to your ATM machine!" << endl;
+
+    do
+    {
+        cout << endl << "Please indicate your user-type:" << endl;
+        cout << "1 - Customer\n2 - Manager\n" << endl;
+        cin >> type;
+
+        if(type == 1 || type == 2){
+            new_user.SetUserType(type);
+            user_type = true;
+        }
+        else{
+            cout << "Error: Not a User Type!" << endl;
+        }
+
+    }while(!user_type);
+
+    cout<<"Please enter User credentials"<<endl;
+
+    while(!user){
+
+        if(type == 1){
+
+        }
+        else{
+
+        }
+
+        cout << "Username: ";
+        cin >> new_user.username;
+        cout << "\nPassword: ";
+        cin >> new_user.password;
+
+
+    }
 
     return 0;
 }
