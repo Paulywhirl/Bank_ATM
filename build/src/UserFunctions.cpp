3142acba5558055566d4c4116d87e7425b42384d
Paul Henderson <phender9@uwo.ca>
2016-10-22 17:41:22 -0400

    customer functions complete (to some extent)

--- build/src/UserFunctions.cpp
+++ build/src/UserFunctions.cpp
@@ -11,6 +11,8 @@
 
 UserFunctions::UserFunctions() {}
 
+
+//********************************************************************************************************
 void UserFunctions:: startUp(string f_name, int type) {
 
     User *user, current_user;
@@ -20,7 +22,6 @@ void UserFunctions:: startUp(string f_name, int type) {
 
         if(type ==1) {
             findAccount(f_name, user);
-            cout << "third\n";
             cout << "f: " << current_user.getFirstname();
             cout << "\nl: " << current_user.getLastname();
             cout << "\nc: " << setprecision(2) << fixed << current_user.isChecking();
@@ -29,86 +30,302 @@ void UserFunctions:: startUp(string f_name, int type) {
 
         }
         if(type ==2){
-
+            managerMenu(user);
         }
 
     }
 }
-
-void UserFunctions::customerMenu(User *user){
-    while (logged_on) {
-        int option;
-        cout << "\n*************************************\n";
-        cout << "* 1 - Withdraw     2 - Deposit      *\n";
-        cout << "* 3 - Transfer     4 - Logout       *\n";
+//********************************************************************************************************
+void UserFunctions::managerMenu(User *user){
+    do {
+        int optionmenu;
+        cout << "\n***********************************************\n";
+        cout << "* 1 - Add Customer     2 - Delete Customer      *\n";
+        cout << "* 3 - View Customer     4 - Logout       *\n";
         cout << "*************************************\n\n";
-        cin >> option;
+        optionmenu = getUserFChoice();
 
-        switch (option) {
+        switch (optionmenu) {
             case 1:
-                cout << "testing withdraw";
-                withdraw(user);
+                addCustomer();
+                cin.clear();
                 break;
             case 2:
-                cout << "testing deposit";
                 deposit(user);
                 break;
             case 3:
-                cout << "testing transfer";
                 transfer(user);
+                break;
             case 4:
-                cout << "testing logging out";
-                logout();
+                logout(user);
                 logged_on =false;
+                break;
+            default: {cout << "\nInvalid input\n"; break;}
         }
-    }
+    }while (logged_on);
     return;
 }
+//********************************************************************************************************
+
+void UserFunctions::addCustomer(){
+
+}
+
+void UserFunctions::deleteCustomer(){
+
+}
+
+void UserFunctions::viewCustomer(){
+    
+}
+
+//********************************************************************************************************
+
+void UserFunctions::customerMenu(User *user){
+
+    do {
+        int optionmenu;
+        cout << "\n******************************************\n";
+        cout << "* 1 - Withdraw     2 - Deposit             *\n";
+        cout << "* 3 - Transfer     4 - Print Account       *\n";
+        cout << "* 5 - Logout                               *\n";
+        cout << "********************************************\n\n";
+        optionmenu = getUserFChoice();
+
+        switch (optionmenu) {
+            case 1:
+                withdraw(user);
+                cin.clear();
+                break;
+            case 2:
+                deposit(user);
+                break;
+            case 3:
+                transfer(user);
+                break;
+            case 4:
+                cout << "**************************" << endl;
+                cout << "* Name: "<<line[0]+" "+line[1]+"*"<< endl;
+                cout << "*Checking Account: " <<line[3]+"*"<< endl;
+                cout << "*Savings Account: " <<line[5]+" *"<< endl;
+                cout << "***************************" << endl;
+                break;
+            case 5:
+                logout(user);
+                logged_on =false;
+                break;
+            default: {cout << "\nInvalid input\n"; break;}
+        }
+    }while (logged_on);
+    return;
+}
+//********************************************************************************************************
 
 void UserFunctions::withdraw(User *user){
 
-    int option; bool withdrawing = true;
+    int option;
+    bool withdrawing = true;
     while (withdrawing) {
         cout << "*******************************\n";
         cout << "* 1 - Checkings    2 - Savings*\n";
         cout << "* 3 - Exit                    *\n";
         cout << "*******************************\n";
-        cin >> option;
+        option = getUserFChoice();
 
         switch (option) {
-            case 1:
-                int amount;
-                cout <<"Enter Amount:  ";
+            case 1: {
+                double amount;
+                cout << "Enter Amount, must be whole number: ";
                 cin >> amount;
-                user->depositCheck(amount, user);
-                break;
-            case 2:
-                break;
-            case 3:
+                user->withdrawCheck(amount, user);
+                cin.clear();
                 withdrawing = false;
                 break;
+            }
+            case 2: {
+                double amount;
+                cout << "Enter Amount, must be whole number: ";
+                cin >> amount;
+                user->withdrawSave(amount, user);
+                cin.clear();
+                withdrawing=false;
+                break;
+            }
+            case 3: {
+                cin.clear();
+                withdrawing = false;
+                break;
+            }
+            default: {break;}
         }
 
     }
     return;
 }
+//********************************************************************************************************
 
 void UserFunctions::deposit(User *user){
+    int option;
+    bool depositing = true;
+    while (depositing) {
+        cout << "*******************************\n";
+        cout << "* 1 - Checkings    2 - Savings*\n";
+        cout << "* 3 - Exit                    *\n";
+        cout << "*******************************\n";
+        option = getUserFChoice();
 
+        switch (option) {
+            case 1: {
+                double amount;
+                cout << "Enter Amount to deposit, must be whole number: ";
+                cin >> amount;
+                user->depositCheck(amount, user);
+                cin.clear();
+                depositing = false;
+                break;
+            }
+            case 2: {
+                double amount;
+                cout << "Enter Amount to deposit:  ";
+                cin >> amount;
+                user->depositSave(amount, user);
+                cin.clear();
+                depositing=false;
+                break;
+            }
+            case 3: {
+                cin.clear();
+                depositing = false;
+                break;
+            }
+            default: {break;}
+        }
+
+    }
+    return;
 }
+//********************************************************************************************************
 
 void UserFunctions::transfer(User *user){
+    int option;
+    bool depositing = true;
+    while (depositing) {
+        cout << "*******************************\n";
+        cout << "* 1 - Checkings to Savings    *\n";
+        cout << "* 2 - Savings to Checkings    *\n";
+        cout << "* 3 - Exit                    *\n";
+        cout << "*******************************\n";
+        option = getUserFChoice();
 
+        switch (option) {
+            case 1: {
+                double amount;
+                cout << "Enter Amount to transfer:  ";
+                cin >> amount;
+                user->transferCtoS(amount, user);
+                cin.clear();
+                depositing = false;
+                break;
+            }
+            case 2: {
+                double amount;
+                cout << "Enter Amount to transfer:  ";
+                cin >> amount;
+                user->transferStoC(amount, user);
+                cin.clear();
+                depositing=false;
+                break;
+            }
+            case 3: {
+                cin.clear();
+                depositing = false;
+                break;
+            }
+            default: {break;}
+        }
+
+    }
+    return;
 }
+//********************************************************************************************************
 
-void UserFunctions:: logout(){
+void UserFunctions:: logout(User *user){
 
+    double check = user->isChecking();
+    double save = user->isSaving();
+    stringstream Scheck, Ssave;
+    Scheck << fixed << setprecision(2) << check;
+    string c = Scheck.str();
+    Ssave << fixed << setprecision(2) << save;
+    string s = Ssave.str();
+
+    string new_line = user->getFirstname() + " " + user->getLastname()
+    + " checking " + c + " saving " + s;
+
+    string replace_line = line[0] + " " + line[1] + " " + line[2] + " " + line[3] + " " + line[4] + " " + line[5];
+
+    cout << new_line;
+
+    ifstream Filein("C:\\Users\\Owner\\Documents\\Third Year\\Comp Sci 3307\\Bank_ATM\\build\\res\\Acc_Info");
+    ofstream Fileout("temporary");
+
+    if(!Filein || !Fileout){
+        cout << "Error updating profile due to files missing or files not opening";
+        return;
+    }
+
+    string line;
+    while(!Filein.eof()){
+        getline(Filein, line);
+        if(replace_line == line){
+            Fileout << new_line << endl;
+        }
+        else{
+            Fileout << line << endl;
+        }
+    }
+
+    Filein.close();
+    Fileout.close();
+
+    rewrite();
+    return;
 }
+//********************************************************************************************************
+
+void UserFunctions::rewrite() {
+    ofstream Filechange("C:\\Users\\Owner\\Documents\\Third Year\\Comp Sci 3307\\Bank_ATM\\build\\res\\Acc_Info");
+    ifstream Filetemp("temporary");
+
+    if(!Filechange || !Filetemp){
+        cout << "Error updating profile due to files missing or files not opening";
+        return;
+    }
+
+    string line_to_change;
+    while(!Filechange.eof()) {
+        getline(Filetemp, line_to_change);
+        Filechange << line_to_change << endl;
+        if(line_to_change == ""){
+            break;
+        }
+    }
+
+    Filechange.close();
+    Filetemp.close();
+
+    ofstream ofs;
+    ofs.open("temporary", std::ofstream::out | std::ofstream::trunc);
+    ofs.close();
+    return;
+}
+//********************************************************************************************************
+
 
 void UserFunctions::findAccount(string f_name, User *user) {
 
-    string line[100], the_line;
-
+    string the_line;
+    int i=0;
 
     ifstream customerFileR;
     customerFileR.open("C:\\Users\\Owner\\Documents\\Third Year\\Comp Sci 3307\\Bank_ATM\\build\\res\\Acc_Info");
@@ -119,23 +336,24 @@ void UserFunctions::findAccount(string f_name, User *user) {
             getline(customerFileR, the_line);
 
             stringstream ssin(the_line);
-            for (int i; ssin.good() && i < 7; i++) {
+            while (ssin.good() && i < 8) {
                 ssin >> line[i];
+                ++i;
             }
-            if (line[1] == f_name){
+            if (line[0] == f_name){
                         break;
                 }
-
+            i=0;
         }
 
-        string check = line[4], save = line[6];
+        string check = line[3], save = line[5];
         double checekr = stod(check);
         cout << checekr << "\n";
         double saver = stod(save);
         cout << saver;
 
         user->setFirstname(f_name);
-        user->setLastname(line[2]);
+        user->setLastname(line[1]);
         user->setChecking(checekr);
         user->setSaving(saver);
 
@@ -144,4 +362,11 @@ void UserFunctions::findAccount(string f_name, User *user) {
     }
     return;
 
+}
+//********************************************************************************************************
+
+int UserFunctions::getUserFChoice(){
+    int choice =0;
+    cin >> choice;
+    return choice;
 }
